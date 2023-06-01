2270fe685368aa22203a8ac86447be7353fdd9ec
Brian E. McElaney <brian.e.mcelaney@gmail.com>
2023-06-01 14:39:11 -0400

    Add mix project

---
+++ lib/phx_gen_customize.ex
@@ -0,0 +1,18 @@
+defmodule PhxGenCustomize do
+  @moduledoc """
+  Documentation for `PhxGenCustomize`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PhxGenCustomize.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/phx_gen_customize_test.exs
@@ -0,0 +1,8 @@
+defmodule PhxGenCustomizeTest do
+  use ExUnit.Case
+  doctest PhxGenCustomize
+
+  test "greets the world" do
+    assert PhxGenCustomize.hello() == :world
+  end
+end
